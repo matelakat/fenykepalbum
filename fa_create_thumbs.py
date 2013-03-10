e7b420dfb9c44ca391cbeaeaecf091a598a5904d
Mate Lakat <mlakat@mlakat.hu>
2013-03-10 19:16:24 +0000

    thumb size could be specified

--- fa_create_thumbs.py
+++ fa_create_thumbs.py
@@ -9,8 +9,9 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 
-def save_thumb(stream, repo):
-    cmd = "convert -resize 100x100 -auto-orient - gif:-"
+def save_thumb(stream, repo, size):
+    size_option = "%sx%s" % (size, size)
+    cmd = "convert -resize %s -auto-orient - gif:-" % size_option
     proc = subprocess.Popen(cmd.split(), stdin=stream, stdout=subprocess.PIPE)
     out, err = proc.communicate()
     return repo.save(StringIO.StringIO(out))
@@ -22,14 +23,15 @@ def to_stream(obj):
     return open(fname, "rb")
 
 
-def create_thumbs(source, dest):
+def create_thumbs(source, dest, size):
+    size = int(size)
     src = repository.Repository(fs.Directory(source))
     dst = repository.Repository(fs.Directory(dest))
 
     for picture in picture_lib.picture_objects(src):
         with to_stream(picture) as picture_stream:
             logging.info("saving thumb for %s", picture.checksum)
-            thumb = save_thumb(picture_stream, dst)
+            thumb = save_thumb(picture_stream, dst, size)
 
             if thumb.metadata:
                 logging.info("potential duplicate thumb")
@@ -38,7 +40,7 @@ def create_thumbs(source, dest):
                 metadata = dict(thumbnail=dict())
 
             metadata['thumbnail'][picture.checksum] = eval(picture.metadata)
-            metadata['thumbnail']['size'] = 100
+            metadata['thumbnail']['size'] = size
             metadata['thumbnail']['format'] = 'gif'
 
             thumb.metadata = repr(metadata)
@@ -48,9 +50,10 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Create thumbnails')
     parser.add_argument('source', help='Source filestore')
     parser.add_argument('dest', help='Destination filestore')
+    parser.add_argument('--size', help='maximal size of thumb', default='100')
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
-    create_thumbs(args.source, args.dest)
+    create_thumbs(args.source, args.dest, args.size)
